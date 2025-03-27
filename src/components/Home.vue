e31d1d37e80af9b2bd8f4fd04e4cc8bd3210f644
anderson <andersondjame@gmail.com>
2025-03-27 14:56:05 +0100

    first commit project vue3

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <p>Ceci est un paragraphe test.</p>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+    const message = ref('Hello World!')
+</script>
+
+<style scoped>
+
+</style>
